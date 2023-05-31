184632c547afead72cd6a23bd4821a1848b6d897
jiekai <jiekai22s@forwardschool.edu.my>
2023-05-31 20:59:16 +0800

    update 5/31

--- pages/home.php
+++ pages/home.php
@@ -4,14 +4,22 @@
 ?>
 <div class="container my-5 mx-auto" style="max-width: 700px;">
     <h1 class="h1 mb-4 text-center">Customer Service Feedback Form</h1>
-    <?php require "parts/message_warning.php";?>
-    <?php if(empty($_SESSION['warning'])):?>
-        <div class="card p-4">
-            <?php require dirname(__DIR__) .  '/parts/message_error.php'; ?>
-            <?php require dirname(__DIR__) .  '/parts/message_success.php'; ?>
-            <?php require dirname(__DIR__) .  '/parts/questions.php'; ?>
-        </div>
-    <?php endif;?>
+        <?php if(isset( $_SESSION['warning'])):?>
+            <div class="card p-4 mb-3">
+                <div class="text-center">
+                    <p class="fs-4 "><?= $_SESSION['warning']; ?></p>
+                    <a href="/login" type="button" class="btn btn-primary ">Login</a>
+                    <a href="/signup" type="button" class="btn btn-primary ">Sign Up</a>
+                </div>
+                <?php unset ($_SESSION['warning']); ?>
+            </div>
+        <?php else :?>
+            <div class="card p-4">
+                <?php require dirname(__DIR__) .  '/parts/message_error.php'; ?>
+                <?php require dirname(__DIR__) .  '/parts/message_success.php'; ?>
+                <?php require dirname(__DIR__) .  '/parts/questions.php'; ?>
+            </div> 
+        <?php endif ;?>
     <?php if(isUser()):?>
         <div class="text-center mt-4">
             <a href="/results" class="btn btn-inverse">View Results</a>
